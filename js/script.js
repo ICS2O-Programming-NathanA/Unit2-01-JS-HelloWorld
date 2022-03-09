50352c55ea78980a5a561f41bda8740eb2a52e89
Nathan Araujo <99274283+nathan-araujo@users.noreply.github.com>
2022-03-09 16:05:29 +0000

    added JS, CSS and html files

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Copyright (c) 2022 Nathan All rights reserved
+//
+// Created by: Nathan
+// Created on: Mar 2022
+// This file contains the JS functions for index.html
+
+function myButtonClicked () {
+  // this function displays "Hello, World!"
+  
+  document.getElementById('hello-world').innerHTML = 'Hello, Ms raffin!'
+}
